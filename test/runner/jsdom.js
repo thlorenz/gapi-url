f68ac3f037d7ef254ed2e05dff08dd901618a476
Thorsten Lorenz <thlorenz@gmx.de>
2018-01-10 18:49:29 -0500

    test: cli tests running and exiting non 0 on failure

--- test/runner/jsdom.js
+++ test/runner/jsdom.js
@@ -1,16 +1,44 @@
 'use strict'
 
-const fs = require('fs')
-const path = require('path')
 const jsdom  = require('jsdom')
 const JSDOM = jsdom.JSDOM
 
-const scriptPath = path.join(__dirname, '..', '..', 'build', 'bundle.js')
-const script = fs.readFileSync(scriptPath, 'utf8')
+const browserify = require('browserify')
+const testPath = require.resolve('../gapi-url')
 
-const dom = new JSDOM(
-    '<html><head><script>' + script + '</script></head><body></body></html>'
-  , { runScripts: 'dangerously', resources: 'usable' }
-)
+const b = browserify()
+b.add(testPath)
+b.bundle(runTest)
 
-console.log(dom.window.document.head.children.length)
+const consoleIntercept = {}
+var lastLine = null
+consoleIntercept.error = function consoleError(s) {
+  console.error.apply(console, arguments)
+}
+consoleIntercept.log = function consoleLog(s) {
+  if (s != null && s.trim().length > 0) lastLine = s
+  console.log.apply(console, arguments)
+}
+
+function runTest(err, buf) {
+  const script = buf.toString()
+  if (err != null) {
+    process.exitCode = -1
+    return
+  }
+  const virtualConsole = new jsdom.VirtualConsole()
+  virtualConsole.sendTo(consoleIntercept)
+
+  // eslint-disable-next-line no-new
+  new JSDOM(
+      '<html><head><script>' + script + '</script></head><body></body></html>'
+    , { runScripts: 'dangerously', resources: 'usable', virtualConsole: virtualConsole }
+  )
+}
+
+process.on('exit', onprocessExit)
+function onprocessExit(code) {
+  if (code !== 0) return
+  if (/# *ok/.test(lastLine)) return
+  process.exitCode = 1
+}
